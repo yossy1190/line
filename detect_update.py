8baa69e21aeb7d1bb3315b8e4ada82fe81ce2c33
yossy1190 <69608729+yossy1190@users.noreply.github.com>
2022-04-07 17:51:57 +0900

    Update detect_update.py

--- detect_update.py
+++ detect_update.py
@@ -9,10 +9,14 @@ def detect_update():
     url="https://www.statusparty.jp/schedule/tokyo/ginza/13019/"
     res=requests.get(url)
     soup=BeautifulSoup(res.text,"html.parser")
-    new_elems=soup.select(".type_acceptable")
-    new_elem=str(new_elems[1])
-    print(new_elem)
-
+    
+    try:
+        femail=soup.select(".female")
+        new_elem=str(soup.select(".type_only_remaining"))
+        print(new_elem)
+    except:
+        new_elem=""
+        
     try:
         with open("old_elem.txt") as f:
             old_elem=f.read()
@@ -21,13 +25,14 @@ def detect_update():
 
     if new_elem==old_elem:
         print('更新はありません')
-        line.main(f"更新はありません。リンクを確認してください。\n{url}")
+        line.main(f"更新はありません。")
         return True
+    
     else:
         with open("old_elem.txt","w") as f:
-            old_elem=f.write(new_elem)
-        print('更新されました')
-        line.main(f"もうすぐ満席です。")
+            f.write(new_elem)
+        print('もうすぐ満席です。')
+        line.main(f"もうすぐ満席です。リンクを確認してください。\n{url}")
         return False
     
 detect_update()
